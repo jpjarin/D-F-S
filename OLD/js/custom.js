15b6f95ae2c70154a6587ab843782b4072f0c318
Jan Paolo Viray Jarin <37213356+jpjarin@users.noreply.github.com>
2018-09-09 21:17:13 +0800

    new commit

--- OLD/js/custom.js
+++ OLD/js/custom.js
@@ -2,13 +2,12 @@
 
 	new WOW().init();
 	
-	$(".navbar").hide();
 	
 	// Fade in .navbar
 	$(function () {
 		$(window).scroll(function () {
             // set distance user needs to scroll before we fadeIn navbar
-			if ($(this).scrollTop() > 500) {
+			if ($(this).scrollTop() > 100) {
 				$('.navbar').fadeIn();
 			} else {
 				$('.navbar').fadeOut();
@@ -17,14 +16,6 @@
 
 	
 	});
-	$(window).bind('scroll', function() {
-        var navHeight = $(window).height() - 100;
-        if ($(window).scrollTop() > navHeight) {
-            $('.navbar-default').addClass('on');
-        } else {
-            $('.navbar-default').removeClass('on');
-        }
-    });
 
     $('body').scrollspy({ 
         target: '.navbar',
